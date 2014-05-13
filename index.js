587faec871dac06243ca6eb8ff79dcadfb321377
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-13 22:30:10 +1000

    Updates to work with latest orientdb

--- index.js
+++ index.js
@@ -1,95 +1,109 @@
-var async = require('async'),
-    debug = require('debug')('graphdb-orient'),
-    orientDebug = require('debug')('orientdb'),
-    errors = {
-        NOT_CONNECTED: 'Unable to perform operation on disconnected db'
-    },
-    orienteer = require('orienteer'),
-    commands = {},
-    _ = require('underscore'),
-    templates = {
-        selectById: 'SELECT FROM <%= type %> WHERE id = "<%= id %>"',
-        selectEdge: 'SELECT FROM <%= type %> ' +
-            'WHERE in.id = "<%= source.id %>" ' +
-            'AND out.id = "<%= target.id %>"',
+var async = require('async');
+var debug = require('debug')('graphdb-orient');
+var orientDebug = require('debug')('orientdb');
+var orienteer = require('orienteer');
+var commands = {};
+var _ = require('underscore');
 
-        update: 'UPDATE <%= type %> <%= sqlsets %> WHERE id = "<%= id %>"',
-        vertexCreate: 'CREATE VERTEX <%= type %> <%= sqlsets %>',
-        edgeCreate: 'CREATE EDGE <%= type %> FROM ' +
-            '(SELECT FROM <%= source.type %> WHERE id = "<%= source.id %>") TO ' +
-            '(SELECT FROM <%= target.type %> WHERE id = "<%= target.id %>") ' +
-            '<%= sqlsets %>'
-    };
+/**
+  # graphdb-orient
+
+  OrientDB adapter for the graphdb module.
+
+**/
+
+var DEFAULT_TYPE_VERTEX = 'V';
+var DEFAULT_TYPE_EDGE = 'E';
+
+var errors = {
+  NOT_CONNECTED: 'Unable to perform operation on disconnected db'
+};
+
+var templates = {
+  selectById: 'SELECT FROM <%= type %> WHERE _id = "<%= id %>"',
+  selectEdge: 'SELECT FROM <%= type %> ' +
+  'WHERE in._id = "<%= source.id %>" ' +
+  'AND out._id = "<%= target.id %>"',
+
+  update: 'UPDATE <%= type %> <%= sqlsets %> WHERE _id = "<%= id %>"',
+  vertexCreate: 'CREATE VERTEX <%= type %> <%= sqlsets %>',
+  edgeCreate: 'CREATE EDGE <%= type %> FROM ' +
+  '(SELECT FROM <%= source.type %> WHERE _id = "<%= source.id %>") TO ' +
+  '(SELECT FROM <%= target.type %> WHERE _id = "<%= target.id %>") ' +
+  '<%= sqlsets %>'
+};
 
 // compile each of the templates
 _.each(templates, function(value, key) {
-    templates[key] = _.template(value);
+  templates[key] = _.template(value);
 });
 
 /* define base types handler */
 
 exports.defineBaseTypes = function(types) {
-    debug('defining core types for orientdb connection');
+  debug('defining core types for orientdb connection');
 
-    types.define('string');
-    types.define('uuid').alias('string');
-    types.define('integer');
-    types.define('float');
+  types.define('string');
+  types.define('uuid').alias('string');
+  types.define('integer');
+  types.define('float');
+  types.define('boolean');
+  types.define('date');
 
-    // define some of the orient list and set types
-    types.define('list').alias('embeddedlist');
-    types.define('set').alias('embeddedset');
+  // define some of the orient list and set types
+  types.define('list').alias('embeddedlist');
+  types.define('set').alias('embeddedset');
 };
 
 /**
 ## connect(graph, opts, callback)
 */
 exports.connect = function(graph, opts, callback) {
-    var connection;
+  var connection;
 
-    // ensure we have valid opts
-    opts = opts || {};
+  // ensure we have valid opts
+  opts = opts || {};
 
-    // wrap the callback to create a "debug" back
-    callback = debuggable(callback);
+  // wrap the callback to create a "debug" back
+  callback = debuggable(callback);
 
-    // if we don't have server configuration details, trigger a callback
-    if (! opts.protocol) {
-        return callback(new Error('server connection protocol required to use orientdb connector'));
+  // if we don't have server configuration details, trigger a callback
+  if (! opts.protocol) {
+    return callback(new Error('server connection protocol required to use orientdb connector'));
+  }
+
+  if (! opts.db) {
+    return callback(new Error('db name, username and password require to use orientdb connector'));
+  }
+
+  // initialise the connection
+  connection = orienteer(opts);
+
+  // check that the required database exists
+  connection.dbExist({ name: opts.db }, function(err, result) {
+    if (err || (! result) || (! result.exists)) {
+      return new callback(err || new Error('Could not find db: ' + opts.db));
     }
 
-    if (! opts.db) {
-        return callback(new Error('db name, username and password require to use orientdb connector'));
-    }
+    // update the connection to use the admin user by default
+    graph._connection = connection = connection.as('admin');
 
-    // initialise the connection
-    connection = orienteer(opts);
-
-    // check that the required database exists
-    connection.dbExist({ name: opts.db }, function(err, result) {
-        if (err || (! result) || (! result.exists)) {
-            return new callback(err || new Error('Could not find db: ' + opts.db));
-        }
-
-        // update the connection to use the admin user by default
-        graph._connection = connection = connection.as('admin');
-
-        // otherwise, use the db and trigger the callback
-        connection.db(opts.db);
-        callback();
-    });
+    // otherwise, use the db and trigger the callback
+    connection.db(opts.db);
+    callback();
+  });
 };
 
 /**
 ## close(graph, callback)
 */
 exports.close = function(graph, callback) {
-    // if we have no db, then return the callback
-    if (! graph._connection) return callback();
+  // if we have no db, then return the callback
+  if (! graph._connection) return callback();
 
-    // TODO: close the connection
-    graph._connection = undefined;
-    callback();
+  // TODO: close the connection
+  graph._connection = undefined;
+  callback();
 };
 
 /* operation definitions */
@@ -101,7 +115,8 @@ The active node type is used to ensure the specified type definition exists
 within OrientDB and inherits from the `OGraphVertex` class (which is aliased to `V`).
 */
 exports.activateNodeType = function(graph, definition, callback) {
-    activateType(graph, definition, 'OGraphVertex', callback);
+  debug('activating node type: ', definition);
+  activateType(graph, definition, DEFAULT_TYPE_VERTEX, callback);
 };
 
 /**
@@ -110,107 +125,115 @@ exports.activateNodeType = function(graph, definition, callback) {
 The activateEdgeType function is used to ensure the type has exists in OrientDB.
 */
 exports.activateEdgeType = function(graph, definition, callback) {
-    activateType(graph, definition, 'OGraphEdge', callback);
+  debug('activating edge type: ', definition);
+  activateType(graph, definition, DEFAULT_TYPE_EDGE, callback);
+};
+
+exports.remapId = function(data) {
+  return data._id;
 };
 
 /**
 ## find(graph, searchParams, opts, callback)
 */
 exports.find = function(graph, searchParams, opts, callback) {
-    // if an id has been requested and a type specified, then do a get by id
-    if (searchParams.id && searchParams.type) {
-        debug('searching by id, using type: ' + searchParams.type);
-        return findById(graph, searchParams.id, searchParams.type, callback);
-    }
-    // otherwise, if we have an id, but no type look for a graphvertex
-    // and then a graph edge if no vertex is found
-    else if (searchParams.id) {
-        // first look for a vertex
-        debug('searching by id but no type, looking for a vertex first');
-        findById(graph, searchParams.id, 'OGraphVertex', function(err, results) {
-            if (err || results.length > 0) return callback(err, results);
+  // if an id has been requested and a type specified, then do a get by id
+  if (searchParams.id && searchParams.type) {
+    debug('searching by id, using type: ' + searchParams.type);
+    return findById(graph, searchParams.id, searchParams.type, callback);
+  }
+  // otherwise, if we have an id, but no type look for a graphvertex
+  // and then a graph edge if no vertex is found
+  else if (searchParams.id) {
+    // first look for a vertex
+    debug('searching by id but no type, looking for a vertex first');
+    findById(graph, searchParams.id, DEFAULT_TYPE_VERTEX, function(err, results) {
+      if (err || results.length > 0) return callback(err, results);
 
-            // no hit on the vertex, so look for an edge
-            debug('could not locate vertex, looking for an edge');
-            return findById(graph, searchParams.id, 'OGraphEdge', callback);
-        });
-    }
+      // no hit on the vertex, so look for an edge
+      debug('could not locate vertex, looking for an edge');
+      return findById(graph, searchParams.id, DEFAULT_TYPE_EDGE, callback);
+    });
+  }
     else {
-        return callback(null, []);
+      return callback(null, []);
     }
 };
 
 exports.getEdge = function(graph, source, target, edgeType, callback) {
-    var command = templates.selectEdge({
-            type: edgeType || 'OGraphEdge',
-            source: source,
-            target: target
-        });
+  var command = templates.selectEdge({
+    type: edgeType || DEFAULT_TYPE_EDGE,
+    source: source,
+    target: target
+  });
 
-    // if we don't have a db connection, abort the operation
-    if (! graph._connection) return callback(errors.NOT_CONNECTED);
+  // if we don't have a db connection, abort the operation
+  if (! graph._connection) return callback(errors.NOT_CONNECTED);
 
-    // look for the node details
-    sendCommand(graph._connection, command, function(err, results) {
-        callback(err, (results || [])[0]);
-    });
+  // look for the node details
+  debug('looking for edge: ', command);
+  sendCommand(graph._connection, command, function(err, results) {
+    callback(err, (results || [])[0]);
+  });
 };
 
 /**
 ## saveNode(graph, node, callback)
 */
 exports.saveNode = function(graph, node, callback) {
-    // if we don't have a db connection, abort the operation
-    if (! graph._connection) return callback(new Error(errors.NOT_CONNECTED));
+  // if we don't have a db connection, abort the operation
+  if (! graph._connection) return callback(new Error(errors.NOT_CONNECTED));
 
-    // if we don't have node data, then report an invalid node
-    if (! node.data) return callback(new Error('A node object is require for a save operation'));
+  // if we don't have node data, then report an invalid node
+  if (! node.data) return callback(new Error('A node object is require for a save operation'));
 
-    // if the node type is not defined, then create it
-    node.type = node.type || 'OGraphVertex';
+  // if the node type is not defined, then create it
+  node.type = node.type || DEFAULT_TYPE_VERTEX;
 
-    // look for an existing node
-    findById(graph, node.data.id, node.type, function(err, results) {
-        var existing = (! err) && results.length > 0,
-            commandTemplate = templates[existing ? 'update' : 'vertexCreate'],
-            data = _.omit(node.data, existing ? ['id'] : []),
-                commandText = commandTemplate({
-                    type: node.type,
-                sqlsets: orienteer.objectTo('SET', data),
-                id: existing && results[0].id
-            });
+  // look for an existing node
+  findById(graph, node.data.id, node.type, function(err, results) {
+    var existing = (! err) && results.length > 0,
+        commandTemplate = templates[existing ? 'update' : 'vertexCreate'],
+        data = _.omit(node.data, existing ? ['_id'] : []),
+        commandText = commandTemplate({
+          type: node.type,
+          sqlsets: orienteer.objectTo('SET', data),
+          id: existing && results[0]._id
+        });
 
-        if (err) return callback(err);
+    if (err) return callback(err);
 
-        // run the command
-        sendCommand(graph._connection, commandText, callback);
-    });
+    // run the command
+    sendCommand(graph._connection, commandText, callback);
+  });
 };
 
 exports.saveEdge = function(graph, source, target, entity, callback) {
-    // if we don't have a db connection, abort the operation
-    if (! graph._connection) return callback(new Error(errors.NOT_CONNECTED));
+  // if we don't have a db connection, abort the operation
+  if (! graph._connection) return callback(new Error(errors.NOT_CONNECTED));
 
-    // if we don't have node data, then report an invalid node
-    if (! entity.data) return callback(new Error('A valid entity is require for a save operation'));
+  // if we don't have node data, then report an invalid node
+  if (! entity.data) return callback(new Error('A valid entity is require for a save operation'));
 
-    // look for an existing node
-    exports.getEdge(graph, source, target, entity.type, function(err, existing) {
-        var commandTemplate = templates[existing ? 'update' : 'edgeCreate'],
-            data = _.omit(entity.data, existing ? ['id'] : []),
-            commandText = commandTemplate({
-                type: entity.type || 'OGraphEdge',
-                sqlsets: orienteer.objectTo('SET', data),
-                source: source,
-                target: target,
-                id: existing && existing.id
-            });
+  // look for an existing node
+  exports.getEdge(graph, source, target, entity.type, function(err, existing) {
+    var commandTemplate = templates[existing ? 'update' : 'edgeCreate'],
+        data = _.omit(entity.data, existing ? ['id'] : []),
+        commandText = commandTemplate({
+          type: entity.type || DEFAULT_TYPE_EDGE,
+          sqlsets: orienteer.objectTo('SET', data),
+          source: source,
+          target: target,
+          id: existing && existing.id
+        });
 
-        if (err) return callback(err);
+    if (err) return callback(err);
 
-        // run the command
-        sendCommand(graph._connection, commandText, callback);
-    });
+    debug('saving edge, type: ' + (entity.type || DEFAULT_TYPE_EDGE), commandText);
+
+    // run the command
+    sendCommand(graph._connection, commandText, callback);
+  });
 };
 
 /* internal helper functions */
@@ -223,17 +246,17 @@ properly defined within orient.  Within orient, there is a notion of classes
 that provide some aspect of types.
 */
 function activateType(graph, definition, baseClass, callback) {
-    var className = definition.type;
+  var className = definition.type;
 
-    // if we don't have a db connection, abort the operation
-    if (! graph._connection) return callback(errors.NOT_CONNECTED);
+  // if we don't have a db connection, abort the operation
+  if (! graph._connection) return callback(errors.NOT_CONNECTED);
 
-    // create the class and required properties
-    commands.series([
-        'CREATE CLASS ' + className + ' EXTENDS ' + baseClass,
-        'CREATE PROPERTY ' + className + '.id STRING',
-        'CREATE INDEX ' + className + '.id UNIQUE'
-    ], graph._connection, callback);
+  // create the class and required properties
+  commands.series([
+    'CREATE CLASS ' + className + ' EXTENDS ' + baseClass,
+    'CREATE PROPERTY ' + className + '.id STRING',
+    'CREATE INDEX ' + className + '.id UNIQUE'
+  ], graph._connection, callback);
 }
 
 /**
@@ -242,13 +265,13 @@ function activateType(graph, definition, baseClass, callback) {
 This is simple function that provides debug info on error conditions
 */
 function debuggable(callback) {
-    return function(err) {
-        if (err instanceof Error) {
-            debug('error: ' + err.message);
-        }
+  return function(err) {
+    if (err instanceof Error) {
+      debug('error: ' + err.message);
+    }
 
-        callback.apply(this, arguments);
-    };
+    callback.apply(this, arguments);
+  };
 }
 
 /**
@@ -257,13 +280,13 @@ function debuggable(callback) {
 The findById function is used internally to find a specific object by id.
 */
 function findById(graph, id, className, callback) {
-    var command = templates.selectById({ type: className, id: id });
+  var command = templates.selectById({ type: className, id: id });
 
-    // if we don't have a db connection, abort the operation
-    if (! graph._connection) return callback(errors.NOT_CONNECTED);
+  // if we don't have a db connection, abort the operation
+  if (! graph._connection) return callback(errors.NOT_CONNECTED);
 
-    // look for the node details
-    sendCommand(graph._connection, command, callback);
+  // look for the node details
+  sendCommand(graph._connection, command, callback);
 }
 
 /**
@@ -273,11 +296,11 @@ The sendCommand function is used internally to provide debug tracking on the com
 send to orientdb
 */
 function sendCommand(connection, command, callback) {
-    orientDebug(command);
-    connection.sql(command, function(err, results) {
-        if (err) orientDebug('error: ', err);
-        callback.apply(this, arguments);
-    });
+  orientDebug(command);
+  connection.sql(command, function(err, results) {
+    if (err) orientDebug('error: ', err);
+    callback.apply(this, arguments);
+  });
 }
 
 /**
@@ -291,16 +314,16 @@ the targetdb.
 As per the series command, except the commands are executed in parallel
 */
 ['series', 'parallel'].forEach(function(op) {
-    commands[op] = function(commands, connection, callback) {
-        // create the bound command calls
-        var boundCommands = commands.map(function(command) {
-            return function(commandCallback) {
-                orientDebug(command);
-                connection.sql(command, commandCallback);
-            };
-        });
+  commands[op] = function(commands, connection, callback) {
+    // create the bound command calls
+    var boundCommands = commands.map(function(command) {
+      return function(commandCallback) {
+        orientDebug(command);
+        connection.sql(command, commandCallback);
+      };
+    });
 
-        // execute the commands
-        async[op].call(null, boundCommands, callback);
-    };
+    // execute the commands
+    async[op].call(null, boundCommands, callback);
+  };
 });
